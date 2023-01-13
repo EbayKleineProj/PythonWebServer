3f35fc7947b575d23c8814aab67324cef40af498
EbayKleineProj <122633813+EbayKleineProj@users.noreply.github.com>
2023-01-13 20:33:58 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import socketserver
 def main():
     PORT = 8080
     Handler = http.server.SimpleHTTPRequestHandler
-
+    git_test = 123
     with socketserver.TCPServer(("", PORT), Handler) as httpd:
         print(f"Serving at {PORT}")
         httpd.serve_forever()
